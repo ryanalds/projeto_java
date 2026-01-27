5d400968143b6215b517ea645cade5c0c04625e9
Ryan Almeida <ryanalds21@gmail.com>
2026-01-27 18:45:46 -0300

    correção de bug de seleção

--- src/Main.java
+++ src/Main.java
@@ -21,23 +21,28 @@ public class Main {
             System.out.println("Login realizado com sucesso");
         }
         else {
-            System.out.println("E-mail ou Senhas invalidos");
+            System.out.println("E-mail ou Senha invalidos");
+            return;
         }
 
         // Opções do Menu:
 
         System.out.println("Bem vindo! Digite o número da opção que deseja:");
-        System.out.println(System.lineSeparator() + System.lineSeparator());
+        System.out.println(System.lineSeparator());
         System.out.println("1 --- Comprar");
         System.out.println("2 --- Vender");
 
+
         int escolha = input.nextInt();
-        Tela telaAtual = null;
+        Tela telaAtual;
 
         if (escolha == 1 ){
             telaAtual = new TelaCompras();
-        }else {
+        }else if (escolha == 2) {
             telaAtual = new TelaVendas();
+        }else {
+            System.out.println("Oção invalida");
+            return;
         }
 
         telaAtual.mostrar();
--- src/Tela.java
+++ src/Tela.java
@@ -1,6 +1,4 @@
 public interface Tela {
     void mostrar();
-    void executarOpcao();
-
     void executarOpcao(int opcao);
 }
