dbe13b3c64be1a666beb3d941bfbb8a26d0125de
Ryan Almeida <ryanalds21@gmail.com>
2026-02-02 11:06:47 -0300

    adicionando funcionalidades do Main a branch

--- src/ecomerce/Main.java
+++ src/ecomerce/Main.java
@@ -1,11 +1,14 @@
 package ecomerce;
 
 import auth.AuthService;
-import java.io.IOException;
-import java.util.*;
 import model.*;
+
 import telas.*;
 
+import java.awt.*;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
 public class Main {
 
@@ -89,39 +92,42 @@ public class Main {
         // Loop para o codigo voltar ao menu a depender da escolha do usuario
 
         while (true) {
-            System.out.println("Bem vindo! Digite o número da opção que deseja:");
-            System.out.println(System.lineSeparator());
+            System.out.println();
+            System.out.println("=== MENU ===");
+            System.out.println();
             System.out.println("1 --- Comprar");
             System.out.println("2 --- Vender");
             System.out.println("3 --- Sair");
+            System.out.println();
+            System.out.print("Bem vindo! Digite o número da opção que deseja: ");
 
 
             int escolha = input.nextInt();
             Tela telaAtual;
-            
+
             if (escolha == 1) {
                 telaAtual = new TelaCompras(estoque);
-                
+
                 telaAtual.mostrar();
                 int opcao = input.nextInt();
                 telaAtual.executarOpcao(opcao);
-                
+
             } else if (escolha == 2) {
                 telaAtual = new TelaVendas(estoque);
                 telaAtual.mostrar();
                 int opcao = input.nextInt();
                 telaAtual.executarOpcao(opcao);
-                
+
             } else if ( escolha == 3) {
                 System.out.println("Obrigado e volte sempre!");
                 break;
-                
+
             } else {
                 System.out.println("Oção invalida");
                 continue;
             }
-            
         }
+
         input.close();
     }
         
